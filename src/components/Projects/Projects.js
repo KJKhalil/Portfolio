17e17ca2a08a35d6eb07cf926ce01b1a343c94ae
Khalil <khalil.j.khalil2@gmail.com>
2024-05-06 17:27:30 -0400

    A Couple Comments Didnt Save. Saved Them And Pushing Again

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,3 +1,4 @@
+//Projects On The Navbar Takes You To This Section.
 import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,3 +1,4 @@
+//Technologies On The Navbar Takes You To This Section.
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
